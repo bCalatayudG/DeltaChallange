7b67fefaf3a1748ea6830ee7d76f74ea81a6b1fa
Bernardo Calatayud <bernardo.calatayud@mobileappscompany.us>
2019-01-29 15:27:46 -0500

    Updates

--- app/src/test/java/com/consultants/myapplication/view/post/PostActivityTest.java
+++ app/src/test/java/com/consultants/myapplication/view/post/PostActivityTest.java
@@ -1,9 +1,15 @@
 package com.consultants.myapplication.view.post;
 
+import android.support.v7.widget.RecyclerView;
+
+import com.consultants.myapplication.R;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.robolectric.Robolectric;
+
 import org.robolectric.RobolectricTestRunner;
 
 import static org.junit.Assert.*;
@@ -11,9 +17,35 @@ import static org.junit.Assert.*;
 @RunWith(RobolectricTestRunner.class)
 public class PostActivityTest {
 
+    private PostActivity postActivity;
+
+    @Mock
+    RecyclerView.LayoutManager mockLayoutManager;
+
+    @Mock
+    PostAdapter mockPostAdapter;
 
     @Before
     public void setUp(){
+
+        postActivity = Robolectric.setupActivity(PostActivity.class);
+
+    }
+
+    @Test
+    public void activityExists(){
+        assertNotNull(postActivity);
+    }
+
+    @Test
+    public void defaultDisplay(){
+
+        RecyclerView recyclerView = postActivity.findViewById(R.id.rvMain);
+        //RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+        //assertThat(layoutManager,mockLayoutManager).isEqualTo(mockLayoutManager);
+        recyclerView.setAdapter(mockPostAdapter);
+        assertNotNull(recyclerView);
+
 
     }
 
